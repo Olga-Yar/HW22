3bf59bae729825b164c27ae84829255c9e877f68
Olga Yarosh <O_ll_y_a@mail.ru>
2023-06-12 20:32:32 +0300

    добавлен name в пути

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from catalog.views import index, contact, post_card
 
 urlpatterns = [
-    path('', index),
-    path('contact', contact),
+    path('', index, name='home'),
+    path('contact', contact, name='contact'),
     path('product/<int:pk>/', post_card)
 ]
