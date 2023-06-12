5e3a7b6f39c6bcd1e1fc33c15034c8d10c383fe7
Olga Yarosh <O_ll_y_a@mail.ru>
2023-06-12 17:46:37 +0300

    закомментирование категории в модели Продукт и создание связи с моделью Категории при помощи нового поля в Продукте cat

--- catalog/models.py
+++ catalog/models.py
@@ -7,10 +7,11 @@ class Product(models.Model):
     name = models.CharField(max_length=300, verbose_name='наименование')
     about = models.TextField(blank=True, verbose_name='описание')
     image = models.ImageField(upload_to='products/', verbose_name='превью')
-    category = models.CharField(max_length=300, verbose_name='категория')
+    #category = models.CharField(max_length=300, verbose_name='категория')
     price_lot = models.IntegerField(verbose_name='цена за покупку')
     date_create = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
     date_last_change = models.DateTimeField(auto_now=True, verbose_name='дата последнего изменения')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return f'{self.name}: {self.about}'
@@ -20,7 +21,7 @@ class Product(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=300, verbose_name='наименование')
+    name = models.CharField(max_length=300, verbose_name='наименование', db_index=True)
     about = models.TextField(blank=True, verbose_name='описание')
 
     def __str__(self):
