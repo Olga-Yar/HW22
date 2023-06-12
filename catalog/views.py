5aa10781a8005dc47f9766e686c58055d273e072
Olga Yarosh <O_ll_y_a@mail.ru>
2023-06-12 15:59:19 +0300

    создание контроллеров

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Category, Product
+
 
 # Create your views here.
+def index(request):
+    category_list = Category.objects.all()
+    context = {
+        'category_list': category_list
+    }
+    return render(request, 'catalog/index.html', context)
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(name, email, message)
+
+    return render(request, 'catalog/contact.html')
+
+
+def post_card(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': product
+    }
+    return render(request, 'catalog/product.html', context)
