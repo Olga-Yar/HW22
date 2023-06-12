7da5f7c38a93c8a607e76620dc42d8034a6d5480
Olga Yarosh <O_ll_y_a@mail.ru>
2023-06-12 15:57:59 +0300

    создание моделей

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=300, verbose_name='наименование')
+    about = models.TextField(blank=True, verbose_name='описание')
+    image = models.ImageField(upload_to='products/', verbose_name='превью')
+    category = models.CharField(max_length=300, verbose_name='категория')
+    price_lot = models.IntegerField(verbose_name='цена за покупку')
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    date_last_change = models.DateTimeField(auto_now=True, verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name}: {self.about}'
+
+    class Meta:
+        verbose_name = 'Продукция'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=300, verbose_name='наименование')
+    about = models.TextField(blank=True, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}: {self.about}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
