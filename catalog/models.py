b92641cc69d748c665d46ef227dc84ad8a704a8e
Olga Yarosh <O_ll_y_a@mail.ru>
2023-06-18 19:35:20 +0300

    создание модели Блог

--- catalog/models.py
+++ catalog/models.py
@@ -32,3 +32,20 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=150, verbose_name='заголовок')
+    slug = models.CharField(max_length=255, unique=True, db_index=True, verbose_name='URL')
+    content = models.TextField(blank=True, verbose_name='содержимое')
+    image = models.ImageField(upload_to='Blog/', verbose_name='превью')
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+    is_public = models.BooleanField(default=True, verbose_name='публикация')
+    num_views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.title} - {self.is_public}'
+
+    class Meta:
+        verbose_name = 'Публикация'
+        verbose_name_plural = 'Публикации'
