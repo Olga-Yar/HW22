8e0ca7a19c7942d96af8c3ca6314a84c49e4b2c7
Olga Yarosh <O_ll_y_a@mail.ru>
2023-06-24 17:36:39 +0300

    создание путей для блога

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,16 +3,21 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from catalog.views import index, contact, ProductDitailView, CategoryListView, \
-    ProductListView, ProductCreateView, ProductUpdateView, ProductDeleteView, BlogListView
+    ProductListView, ProductCreateView, ProductUpdateView, ProductDeleteView, BlogListView, BlogDitailView,\
+    BlogCreateView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
     path('', index, name='home'),
     path('contact', contact, name='contact'),
     path('category/', CategoryListView.as_view(), name='category_list'),
     path('product/', ProductListView.as_view(), name='product_list'),
-    path('product/<slug:post_slug>/', ProductDitailView.as_view(), name='product_item'),
+    path('product/<int:pk>/', ProductDitailView.as_view(), name='product_item'),
     path('product/create/', ProductCreateView.as_view(), name='product_create'),
-    path('product/update/<slug:post_slug>/', ProductUpdateView.as_view(), name='product_update'),
-    path('product/delete/<slug:post_slug>/', ProductDeleteView.as_view(), name='product_delete'),
+    path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
+    path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
     path('blog/', BlogListView.as_view(), name='blog_list'),
+    path('blog/<slug:post_slug>/', BlogDitailView.as_view(), name='blog_item'),
+    path('blog/create/', BlogCreateView.as_view(), name='blog_create'),
+    path('blog/<slug:post_slug>/', BlogUpdateView.as_view(), name='blog_update'),
+    path('blog/<slug:post_slug>/', BlogDeleteView.as_view(), name='blog_delete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
