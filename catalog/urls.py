3d94b11f19c8bfc3492610c1425ecc0ec6075984
Olga Yarosh <O_ll_y_a@mail.ru>
2023-06-24 20:56:58 +0300

    добаление пробелов между юрлами

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,11 +10,13 @@ urlpatterns = [
     path('', index, name='home'),
     path('contact', contact, name='contact'),
     path('category/', CategoryListView.as_view(), name='category_list'),
+
     path('product/', ProductListView.as_view(), name='product_list'),
     path('product/<int:pk>/', ProductDitailView.as_view(), name='product_item'),
     path('product/create/', ProductCreateView.as_view(), name='product_create'),
     path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
     path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
+
     path('blog/', BlogListView.as_view(), name='blog_list'),
     path('blog/<slug:post_slug>/', BlogDitailView.as_view(), name='blog_item'),
     path('blog/create/', BlogCreateView.as_view(), name='blog_create'),
