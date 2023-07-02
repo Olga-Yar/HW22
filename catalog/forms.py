9115604db9cdedd9c66a46d7c952e043ace935c6
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-02 20:23:31 +0300

    добавление валидации к формам

--- catalog/forms.py
+++ catalog/forms.py
@@ -9,6 +9,25 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ('name', 'about', 'price_lot', 'cat', 'image')
 
+    def clean_name(self):
+        cleaned_data = self.cleaned_data['name']
+        stop_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
+        if cleaned_data in stop_list:
+            raise forms.ValidationError('Запрещенный продукт.')
+
+        return cleaned_data
+
+    def clean_about(self):
+        cleaned_about = self.cleaned_data['about']
+        stop_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
+        for word in cleaned_about:
+            if word in stop_list:
+                raise forms.ValidationError('Запрещенный продукт.')
+
+        return cleaned_about
+
 
 class BlogForm(forms.ModelForm):
 
