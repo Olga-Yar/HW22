c30eae364e76014d70a838cd4ce04bc7182931bf
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-09 18:49:20 +0300

    добавление класса варификации почты

--- users/views.py
+++ users/views.py
@@ -1,10 +1,17 @@
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.core.exceptions import ValidationError
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from django.utils.http import urlsafe_base64_decode
+from django.views import View
 from django.views.generic import CreateView, UpdateView
 
 from users.forms import UserRegisterForm, UserProfileForm
 from users.models import User
+from users.utils import send_email_verify
+
+from django.contrib.auth.tokens import default_token_generator as token_generator
 
 
 class RegisterView(CreateView):
@@ -12,6 +19,31 @@ class RegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+
+    def post(self, request):
+        send_email_verify(request, User)
+        return redirect('confirm_email')
+
+
+class EmailVerify(View):
+
+    def get(self, request, uidb64, token):
+        user = self.get_user(uidb64)
+
+        if user is not None and token_generator.check_token(user, token):
+            user.email_verify = True
+            login(request, user)
+            return redirect('users:profile')
+        return redirect('users:invalid_verify')
+
+    @staticmethod
+    def get_user(uidb64):
+        try:
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist, ValidationError):
+            user = None
+        return user
 
 
 class ProfileView(UpdateView):
