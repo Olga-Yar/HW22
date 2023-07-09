87371580051e7e6f4f9ec9ea3aea1aa3ea9cbd8d
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-09 18:48:07 +0300

    добавление путей: регистрация, подтверждение почты итд

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,22 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
+from django.views.generic import TemplateView
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView
+from users.views import RegisterView, ProfileView, EmailVerify
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('/logout', LogoutView.as_view(), name='logout'),
+
     path('register/', RegisterView.as_view(), name='register'),
+    path('confirm_email/', TemplateView.as_view(template_name='users/confirm_email.html'), name='confirm_email'),
+    path('verify_email/<uidb64>/<token>/', EmailVerify.as_view(), name='verify_email'),
+
+    path('invalid_verify/', TemplateView.as_view(template_name='invalid_verify.html'), name='invalid_verify'),
+
+    path('/logout', LogoutView.as_view(), name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
 
 ]
