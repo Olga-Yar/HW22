ef9425dfe469d9f28af751544347a69184f5ab5b
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-12 19:30:28 +0300

    добавлены поля в модель пользователь: телефон, аватар, страна

--- users/models.py
+++ users/models.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='почта')
 
-    phone = models.CharField(max_length=40, verbose_name='телефон', null=True)
-    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', null=True)
-    country = models.CharField(max_length=255, verbose_name='страна', null=True)
+    phone = models.CharField(max_length=40, verbose_name='телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    country = models.CharField(max_length=255, verbose_name='страна', **NULLABLE)
 
     email_verify = models.BooleanField(default=False)
 
