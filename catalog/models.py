a933b88728710f8880e9b2243078a41be24d1e98
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-12 19:28:33 +0300

    добавлено поле владелец

--- catalog/models.py
+++ catalog/models.py
@@ -1,20 +1,22 @@
+from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from slugify import slugify
 
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
 
 
 class Product(models.Model):
     name = models.CharField(max_length=300, verbose_name='наименование')
     about = models.TextField(blank=True, verbose_name='описание')
     image = models.ImageField(upload_to='Product/', verbose_name='превью')
-    #category = models.CharField(max_length=300, verbose_name='категория')
     price_lot = models.IntegerField(verbose_name='цена за покупку')
     date_create = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
     date_last_change = models.DateTimeField(auto_now=True, verbose_name='дата последнего изменения')
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, **NULLABLE)
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='владелец')
 
     def __str__(self):
         return f'{self.name}: {self.about}'
