cec3f23fd06e3d02b644d2436f6754300b52817f
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-12 19:33:31 +0300

    добавлен mixin стилей в формы

--- users/forms.py
+++ users/forms.py
@@ -1,21 +1,22 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 
+from catalog.forms import StyleForMixin
 from users.models import User
 
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(StyleForMixin, UserCreationForm):
 
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
 
 
-class UserProfileForm(UserChangeForm):
+class UserProfileForm(StyleForMixin, UserChangeForm):
 
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name', 'phone', 'avatar')
+        fields = ('email', 'first_name', 'last_name', 'phone', 'avatar', 'password')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
