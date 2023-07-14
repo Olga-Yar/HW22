55ad9a65ad61c61ec6ea23de4024575b5d9baa0d
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-14 19:51:16 +0300

    кеширование контроллера относительно одного продукта

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
 from catalog.views import IndexView, contact, ProductDetailView, CategoryListView, \
@@ -16,7 +17,7 @@ urlpatterns = [
     path('category/', CategoryListView.as_view(), name='category_list'),
 
     path('product/', ProductListView.as_view(), name='product_list'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_item'),
+    path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='product_item'),
     path('product/create/', ProductCreateView.as_view(), name='product_create'),
     path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
     path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
