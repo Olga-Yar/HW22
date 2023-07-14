d78cdc9dd8f59f9cc26921dfc3b7573b5c339dc5
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-14 19:50:34 +0300

    низкоуровневое кеширование

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.cache import cache
 from django.forms import inlineformset_factory
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
@@ -7,13 +8,23 @@ from django.views import generic
 
 from catalog.forms import ProductForm, BlogForm, VersionForm
 from catalog.models import Category, Product, Blog, Version
+from config import settings
 
 
 # Create your views here.
 
 class IndexView(generic.View):
-    def get(self, request, *args, **kwargs):
-        category_list = Category.objects.all()
+    def get(self, request):
+
+        if settings.CACHES_ENABLE:
+            key = 'category_list'
+            category_list = cache.get(key)
+            if category_list is None:
+                category_list = Category.objects.all()
+                cache.set(key, category_list)
+        else:
+            category_list = Category.objects.all()
+
         context = {
             'category_list': category_list,
             'title': 'Главная'
