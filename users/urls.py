18cc99fec03c5400ffdb969f213e12935d027c8a
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-15 21:02:22 +0300

    закомичевание urls и создание нового для подтверждения почты

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from django.views.generic import TemplateView
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView, EmailVerify, generate_new_password
+from users.views import RegisterView, ProfileView, ConfirmView, generate_new_password
 
 app_name = UsersConfig.name
 
@@ -11,8 +11,9 @@ urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
 
     path('register/', RegisterView.as_view(), name='register'),
-    path('confirm_email/', TemplateView.as_view(template_name='users/confirm_email.html'), name='confirm_email'),
-    path('verify_email/<uidb64>/<token>/', EmailVerify.as_view(), name='verify_email'),
+    path('confirm_email/<key>/', ConfirmView.as_view(), name='confirm_email'),
+    # path('confirm_email/', TemplateView.as_view(template_name='users/confirm_email.html'), name='confirm_email'),
+    # path('verify_email/<uidb64>/<token>/', EmailVerify.as_view(), name='verify_email'),
 
     path('invalid_verify/', TemplateView.as_view(template_name='users/invalid_verify.html'), name='invalid_verify'),
 
